e1e32c61c89efc578ff631e7f33063d447a9ed73
kicsyromy <kicsyromy@gmail.com>
2019-07-24 09:56:16 +0300

    Add clang-format

--- main.c
+++ main.c
@@ -1,9 +1,27 @@
-#include <stdio.h>
 #include <stdint.h>
+#include <stdio.h>
+
+#include <8052.h>
 
 extern int test(int);
 
-int main()
+void delay(void);
+
+void main(void)
 {
-    return test(5);
+    while (1)
+    {
+        P1 = 0xFF; // Turn ON all LED's connected to Port1
+        delay();
+        P1 = 0x00; // Turn OFF all LED's connected to Port1
+        delay();
+    }
+}
+
+void delay(void)
+{
+    int i, j;
+    for (i = 0; i < 0xff; i++)
+        for (j = 0; j < 0xff; j++)
+            ;
 }
